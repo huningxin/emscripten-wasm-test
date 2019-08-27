c4532ab4a7ab1eb6b92ca821feaf2d3c8c7a6f70
Ningxin Hu <ningxin.hu@intel.com>
2019-08-27 16:35:12 +0800

    Add v_load_deinterleave

---
+++ v_load_interleave.cc
@@ -0,0 +1,56 @@
+#include <wasm_simd128.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <time.h>  
+
+inline v128_t wasm_unpacklo_i8x16(v128_t a, v128_t b) {
+    // expect punpcklbw
+    return wasm_v8x16_shuffle(a, b, 0,16,1,17,2,18,3,19,4,20,5,21,6,22,7,23);
+}
+
+inline v128_t wasm_unpackhi_i64x2(v128_t a, v128_t b) {
+    // expect punpckhqdq
+    return wasm_v8x16_shuffle(a, b, 8,9,10,11,12,13,14,15,24,25,26,27,28,29,30,31);
+}
+
+__attribute__((noinline))
+inline void v_load_deinterleave(const unsigned char* ptr, __u8x16& a, __u8x16& b, __u8x16& c)
+{
+    v128_t t00 = wasm_v128_load(ptr);
+    v128_t t01 = wasm_v128_load(ptr + 16);
+    v128_t t02 = wasm_v128_load(ptr + 32);
+
+    v128_t t10 = wasm_unpacklo_i8x16(t00, wasm_unpackhi_i64x2(t01, t01));
+    v128_t t11 = wasm_unpacklo_i8x16(wasm_unpackhi_i64x2(t00, t00), t02);
+    v128_t t12 = wasm_unpacklo_i8x16(t01, wasm_unpackhi_i64x2(t02, t02));
+
+    v128_t t20 = wasm_unpacklo_i8x16(t10, wasm_unpackhi_i64x2(t11, t11));
+    v128_t t21 = wasm_unpacklo_i8x16(wasm_unpackhi_i64x2(t10, t10), t12);
+    v128_t t22 = wasm_unpacklo_i8x16(t11, wasm_unpackhi_i64x2(t12, t12));
+
+    v128_t t30 = wasm_unpacklo_i8x16(t20, wasm_unpackhi_i64x2(t21, t21));
+    v128_t t31 = wasm_unpacklo_i8x16(wasm_unpackhi_i64x2(t20, t20), t22);
+    v128_t t32 = wasm_unpacklo_i8x16(t21, wasm_unpackhi_i64x2(t22, t22));
+
+    a = wasm_unpacklo_i8x16(t30, wasm_unpackhi_i64x2(t31, t31));
+    b = wasm_unpacklo_i8x16(wasm_unpackhi_i64x2(t30, t30), t32);
+    c = wasm_unpacklo_i8x16(t31, wasm_unpackhi_i64x2(t32, t32));
+}
+
+int main() {
+    const size_t size = 48 * 1000;
+    unsigned char data[size];
+    srand (time(NULL));
+    for (int i = 0; i < size; ++i)
+      data[i] = rand() % 255;;
+    __u8x16 a, b, c;
+    srand (time(NULL));
+    for (int i = 0; i < 100000000; ++i)
+      v_load_deinterleave(data + rand() % 100, a, b, c);
+    printf("%d %d %d \n",
+           wasm_i8x16_extract_lane(a, 0),
+           wasm_i8x16_extract_lane(b, 1),
+           wasm_i8x16_extract_lane(c, 2));
+
+    return 0;
+}
---
+++ v_load_interleave_sse.cc
@@ -0,0 +1,49 @@
+#include <emmintrin.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <time.h>
+
+// gcc v_load_interleave_sse.cc -flax-vector-conversions -msse3 -O2 -g2
+
+void v_load_deinterleave(const unsigned char * ptr, __v16qu& a, __v16qu& b, __v16qu& c) __attribute__((noinline));
+
+void v_load_deinterleave(const unsigned char * ptr, __v16qu& a, __v16qu& b, __v16qu& c)
+{
+    __m128i t00 = _mm_loadu_si128((const __m128i*)ptr);
+    __m128i t01 = _mm_loadu_si128((const __m128i*)(ptr + 16));
+    __m128i t02 = _mm_loadu_si128((const __m128i*)(ptr + 32));
+
+    __m128i t10 = _mm_unpacklo_epi8(t00, _mm_unpackhi_epi64(t01, t01));
+    __m128i t11 = _mm_unpacklo_epi8(_mm_unpackhi_epi64(t00, t00), t02);
+    __m128i t12 = _mm_unpacklo_epi8(t01, _mm_unpackhi_epi64(t02, t02));
+
+    __m128i t20 = _mm_unpacklo_epi8(t10, _mm_unpackhi_epi64(t11, t11));
+    __m128i t21 = _mm_unpacklo_epi8(_mm_unpackhi_epi64(t10, t10), t12);
+    __m128i t22 = _mm_unpacklo_epi8(t11, _mm_unpackhi_epi64(t12, t12));
+
+    __m128i t30 = _mm_unpacklo_epi8(t20, _mm_unpackhi_epi64(t21, t21));
+    __m128i t31 = _mm_unpacklo_epi8(_mm_unpackhi_epi64(t20, t20), t22);
+    __m128i t32 = _mm_unpacklo_epi8(t21, _mm_unpackhi_epi64(t22, t22));
+
+    a = _mm_unpacklo_epi8(t30, _mm_unpackhi_epi64(t31, t31));
+    b = _mm_unpacklo_epi8(_mm_unpackhi_epi64(t30, t30), t32);
+    c = _mm_unpacklo_epi8(t31, _mm_unpackhi_epi64(t32, t32));
+}
+
+int main() {
+    const size_t size = 48 * 1000;
+    unsigned char data[size];
+    srand (time(NULL));
+    for (int i = 0; i < size; ++i)
+      data[i] = rand() % 255;;
+    __v16qu a, b, c;
+    srand (time(NULL));
+    for (int i = 0; i < 100000000; ++i)
+      v_load_deinterleave(data + rand() % 100, a, b, c);
+    printf("%d %d %d \n",
+           _mm_cvtsi128_si32(a),
+           _mm_cvtsi128_si32(b),
+           _mm_cvtsi128_si32(c));
+
+    return 0;
+}
